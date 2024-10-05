667edd1d4205413b7dd640bacb2d1d193bdf110c
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-10-05 13:30:58 +0530

    Neovim Setup 4

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -22,8 +22,6 @@ return {
         t['<C-e>'] = {'scroll', { '0.10', 'false', '50'}} -- Scroll down one line
 
         -- Add smooth scrolling to regular j/k and G/gg
-        t['k'] = {'scroll', {'-vim.wo.scroll', 'true', '100'}} -- Scroll up smoothly with 'k'
-        t['j'] = {'scroll', { 'vim.wo.scroll', 'true', '100'}} -- Scroll down smoothly with 'j'
         t['gg'] = {'scroll', {'-vim.api.nvim_buf_line_count(0)', 'true', '300'}} -- Smooth scroll to top
         t['G'] = {'scroll', { 'vim.api.nvim_buf_line_count(0)', 'true', '300'}} -- Smooth scroll to bottom
         t['H'] = {'scroll', {'-0.30', 'true', '150'}} -- Smooth scroll to top of window
