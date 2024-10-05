ad801b7c3b428b5ac0b3b7f92ff94bbe58b9ca51
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-10-05 13:04:48 +0530

    Neovim Setup 2

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,16 +2,33 @@ return {
   "nvimtools/none-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
+
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.erb_lint,
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.rubocop,
+        -- Golang formatting (gofmt, goimports, golines)
+        null_ls.builtins.formatting.gofmt,            -- Basic Go formatter
+        null_ls.builtins.formatting.goimports,        -- Go formatter with imports organization
+        null_ls.builtins.formatting.golines,          -- Go formatter for line length
+
+        -- Python formatting (black, yapf, isort)
+        null_ls.builtins.formatting.black,            -- Python formatter (PEP8 compliant)
+        null_ls.builtins.formatting.yapf,             -- Alternative Python formatter
+        null_ls.builtins.formatting.isort,            -- Sort Python imports
+
+        -- TypeScript, JavaScript, TSX, JSX formatting (prettier)
+        null_ls.builtins.formatting.prettier.with({
+          filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "tsx", "jsx" }
+        }),
+
+        -- Other formatters you already have (stylua, ruby, etc.)
+        null_ls.builtins.formatting.stylua,           -- Lua formatter
+        null_ls.builtins.diagnostics.erb_lint,        -- Ruby on Rails linter
+        null_ls.builtins.diagnostics.rubocop,         -- Ruby linter
+        null_ls.builtins.formatting.rubocop,          -- Ruby formatter
       },
     })
 
+    -- Keymap to format the current buffer
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
   end,
 }
