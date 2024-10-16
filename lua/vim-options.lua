5f10b7e6240828fd7c6ec44dfb2b5d0436c1827a
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-10-16 10:13:37 +0530

    Neovim Setup 11

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -15,3 +15,37 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.wo.number = true
+-- Enable relative line numbers
+vim.opt.nu = true
+vim.opt.rnu = true
+
+-- Enable auto indenting and set it to spaces
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 2
+--
+-- -- Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
+vim.opt.breakindent = true
+--
+-- -- Enable incremental searching
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+--
+-- -- Disable text wrap
+vim.opt.wrap = false
+--
+-- Set leader key to space
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+--
+-- -- Better splitting
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Set completeopt to have a better completion experience
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+-- Enable the sign column to prevent the screen from jumping
+vim.opt.signcolumn = "yes"
+
+vim.api.nvim_create_user_command("Wc", function(args)
+	vim.cmd("wincmd " .. args.args)
+end, { nargs = 1 })
